d8886e61a10941216bf2685fd3600d4407ac42eb
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2024-03-24 21:38:31 +0545

    g

---
+++ dotNet/program/ConsoleApp1/ConsoleApp1/SecondProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SecondProgram
+    {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Hello,from first program");
+
+        }
+
+    }
+}
